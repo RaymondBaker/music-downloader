20cee775a51f81f8acbb1b55c4216470cbad1c8c
Raymond Baker <raymond_baker@hotmail.com>
2020-06-20 18:30:04 -0400

    made setup better

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
 
-sudo apt install youtube-dl
+sudo apt install python3-pip
+sudo pip3 install youtube-dl
 sudo apt install leiningen
 sudo apt install ffmpeg
+
+mkdir -p ~/Downloads/Music
 
 lein run
